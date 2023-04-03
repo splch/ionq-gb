fbd679d68909a77017a73ec23723511e1d2e2acd
Spencer Churchill <churchill@ionq.co>
2023-04-03 12:44:01 -0700

    improve comments for presentation

--- src/main.c
+++ src/main.c
@@ -1,55 +1,58 @@
+/*
+Game Boy code for IonQ's lunch-n-learn
+
+This code demonstrates the following functionalities:
+    1. Graphics rendering
+    2. Audio playback
+    3. User input handling
+    4. Micro optimizations
+*/
+
 #include <gb/gb.h>     // import Game Boy header file for standard I/O
-#include <stdio.h>     // printf function
 #include <stdlib.h>    // to dyamically allocate arrays
 #include <stdbool.h>   // import types like boolean data type
 #include "ionq_logo.h" // import custom header file for ionq logo graphics
 #include "sprite.h"    // import sprite data
 
+unsigned int spritePositions[2 * MAX_HARDWARE_SPRITES + 1]; // store sprite locations
+unsigned int spriteCount;                                   // keep track of current sprite count
+unsigned int currentFrame;                                  // used to alternate sprite visibility
+
 static void initializeGameBoy();          // initialize the Game Boy hardware
 static void displayLogoGraphics();        // display the IonQ logo graphics
 void playSound();                         // play sound
-void spawnSprite();                       // creates a sprite
-static void flickerSprites(unsigned int); // avoid the 10 sprite per line limit
-void moveSprites(unsigned char);          // moves all sprites by d-pad
+static void spawnSprite();                // creates a sprite
+static void flickerSprites(unsigned int); // surpass the sprite per line limit
+static void moveSprites(unsigned char);   // moves all sprites by d-pad
 void setBackgroundOffsets(bool);          // reset background offsets
 static void handleUserInput();            // handle user input controls
 extern char rand(short);                  // use assembly-optimized rand
 
-unsigned int *spritePositions; // store sprite locations
-unsigned int spriteCount;      // keep track of current sprite count
-unsigned int currentFrame;     // used to alternate sprite visibility
-
 void main()
 {
-    // step 1: initialize the Game Boy
-    initializeGameBoy();
-    // step 2: display graphics
-    displayLogoGraphics();
-    // step 3: play audio
-    playSound();
-    // step 4: reset background offsets
-    setBackgroundOffsets(true);
-    // step 5: check for user input
+    initializeGameBoy();        // step 1: initialize the Game Boy
+    displayLogoGraphics();      // step 2: display graphics
+    playSound();                // step 3: play audio
+    setBackgroundOffsets(true); // step 4: reset background offsets
+
     while (true)
     {
-        handleUserInput();
-        flickerSprites(currentFrame++);
+        handleUserInput();              // step 5: check for user input
+        flickerSprites(currentFrame++); // step 6: micro optimizations
         wait_vbl_done();
     }
-
-    // free memory allocated for array before the program exits
-    free(spritePositions);
 }
 
 static void initializeGameBoy()
 {
     DISPLAY_OFF;                   // turn off display
-    NR52_REG = 0x80;               // sound on
-    NR50_REG = 0x77;               // set volume
-    NR51_REG = 0x11;               // enable sound channels
     SPRITES_8x8;                   // set sprite size to 8x8
     set_sprite_data(0, 1, sprite); // load sprite data into VRAM
     spriteCount = 0;               // set the number of sprites
+    currentFrame = 0;              // set the first frame to 0
+    NR52_REG = 0x80;               // sound on
+    NR50_REG = 0x77;               // set volume
+    NR51_REG = 0x11;               // enable sound channels
     SHOW_SPRITES;                  // show sprite layer
     SHOW_BKG;                      // show background layer
     DISPLAY_ON;                    // turn on display
@@ -78,56 +81,50 @@ void playSound()
 
 void spawnSprite()
 {
-    // Set sprite tile and make it initially invisible
-    set_sprite_tile(spriteCount, 0);
-    set_sprite_prop(spriteCount, get_sprite_prop(spriteCount) & ~S_PRIORITY);
+    set_sprite_tile(spriteCount, 0);                                          // set sprite tile to first in vram
+    set_sprite_prop(spriteCount, get_sprite_prop(spriteCount) & ~S_PRIORITY); // make initially sprite visible
+    move_sprite(spriteCount, SPRITE_CENTER_X, SPRITE_CENTER_Y);               // Move the sprite to the center of the screen
 
-    // Move the sprite to the center of the screen
-    move_sprite(spriteCount, SPRITE_CENTER_X, SPRITE_CENTER_Y);
+    spritePositions[2 * spriteCount] = SPRITE_CENTER_X;     // put new sprite in center x
+    spritePositions[2 * spriteCount + 1] = SPRITE_CENTER_Y; // and center y
 
-    // Update spritePositions array
-    spritePositions = realloc(spritePositions, 2 * (spriteCount + 1) * sizeof(int));
-    spritePositions[2 * spriteCount] = SPRITE_CENTER_X;
-    spritePositions[2 * spriteCount + 1] = SPRITE_CENTER_Y;
-
-    spriteCount++; // Increment sprite count
-    if (spriteCount > MAX_HARDWARE_SPRITES)
-        spriteCount = 0;
+    if (++spriteCount > MAX_HARDWARE_SPRITES) // if increased sprite count is over max
+        spriteCount = 0;                      // reset to 0 to avoid seg faults
 }
 
 void flickerSprites(unsigned int frame)
 {
-    unsigned int startSprite = frame % 2 == 0 ? 0 : MAX_HARDWARE_SPRITES;
+    unsigned int startSprite = frame % 2 == 0 ? 0 : MAX_HARDWARE_SPRITES; // determine visible sprites
 
     for (unsigned int i = 0; i < MAX_HARDWARE_SPRITES; i++)
     {
-        int index = (startSprite + i) % spriteCount;
-        int x = spritePositions[2 * index];
-        int y = spritePositions[2 * index + 1];
-
-        move_sprite(i, x, y);
+        int index = (startSprite + i) % spriteCount; // select sprite from visible group
+        int x = spritePositions[2 * index];          // get its x
+        int y = spritePositions[2 * index + 1];      // and y location
+        move_sprite(i, x, y);                        // move it back
     }
 }
 
 void moveSprites(unsigned char input)
 {
-    int dx = 0;
-    int dy = 0;
-    if (input & J_UP)
-        dy = -8;
-    if (input & J_DOWN)
-        dy = 8;
-    if (input & J_LEFT)
-        dx = -8;
-    if (input & J_RIGHT)
-        dx = 8;
-    for (unsigned int i = 0; i < 40; i++)
+    int dx = 0;          // set the difference of x
+    int dy = 0;          // and y
+    if (input & J_UP)    // if the user pressed up
+        dy = -8;         // move the sprite up one tile
+    if (input & J_DOWN)  // down
+        dy = 8;          // one tile
+    if (input & J_LEFT)  // left
+        dx = -8;         // one tile
+    if (input & J_RIGHT) // right
+        dx = 8;          // one tile
+
+    for (unsigned int i = 0; i < MAX_HARDWARE_SPRITES; i++)
     {
-        int newX = spritePositions[2 * i] + dx;
-        int newY = spritePositions[2 * i + 1] + dy;
-        spritePositions[2 * i] = newX;
-        spritePositions[2 * i + 1] = newY;
-        move_sprite(i, newX, newY);
+        int newX = spritePositions[2 * i] + dx;     // add difference to x
+        int newY = spritePositions[2 * i + 1] + dy; // and y positions
+        spritePositions[2 * i] = newX;              // update sprite x
+        spritePositions[2 * i + 1] = newY;          // and y location
+        move_sprite(i, newX, newY);                 // then move the sprite
     }
 }
 
@@ -135,13 +132,13 @@ void setBackgroundOffsets(bool reset)
 {
     if (!reset)
     {
-        SCX_REG = rand(DIV_REG); // set horizontal background offset to random number
-        SCY_REG = rand(SCX_REG); // set vertical background offset to random number
+        SCX_REG = rand(DIV_REG); // set the horizontal
+        SCY_REG = rand(SCX_REG); // and vertical background offset to random number
     }
     else
     {
-        SCX_REG = 0; // set horizontal background offset to 0
-        SCY_REG = 0; // set vertical background offset to 0
+        SCX_REG = 0; // set the horizontal
+        SCY_REG = 0; // and vertical background offset to 0
     }
 }
 
